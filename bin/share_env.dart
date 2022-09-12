f457c2cddf0248c3fb6ed4e4939c74ac04fcd291
Matatias <matatias97@gmail.com>
2022-09-12 17:12:18 +0700

    init commit

---
+++ bin/share_env.dart
@@ -0,0 +1,5 @@
+import 'package:share_env/share_env.dart' as share_env;
+
+void main(List<String> arguments) {
+  print('Hello world: ${share_env.calculate()}!');
+}
---
+++ test/share_env_test.dart
@@ -0,0 +1,8 @@
+import 'package:share_env/share_env.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
